286b5d848554f7cda09f4ab5553bec44963b9d21
Denis <deni4gt@gmail.com>
2024-09-10 20:19:33 +0200

    Added sum tests js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "vite-deploy",
+  test: {
+    globals: true,
+    environment: "node",
+    setupFiles: "./vite.config.js",
+  },
 });
